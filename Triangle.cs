2cf0fae7bd0e403abe0f4cc6eeabed2d19fa0c86
Orthodoxia <114431239+Giosho@users.noreply.github.com>
2024-10-07 18:38:33 +0300

    Add files via upload

---
+++ Triangle.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Въведи число a -> ");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Въведи число h -> ");
+
+int h = int.Parse(Console.ReadLine());
+
+int sum = a * h / 2;
+
+Console.WriteLine($"Лицето е = {sum}");
