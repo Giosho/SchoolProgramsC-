a5843f20a843ac8643703d870ef1c126482fccce
Giosho <114431239+Giosho@users.noreply.github.com>
2024-10-09 20:31:25 +0300

    Update Triangle.cs

--- Triangle.cs
+++ Triangle.cs
@@ -1,11 +1,11 @@
-﻿Console.WriteLine("Въведи число a -> ");
+Console.WriteLine("Въведи число a -> ");
 
-int a = int.Parse(Console.ReadLine());
+double a = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Въведи число h -> ");
 
-int h = int.Parse(Console.ReadLine());
+double h = double.Parse(Console.ReadLine());
 
-int sum = a * h / 2;
+double sum = a * h / 2;
 
 Console.WriteLine($"Лицето е = {sum}");
